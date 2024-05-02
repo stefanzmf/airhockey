77d94e76afed43ab96eb546374fdd8e66cd41986
StefanZ <stefan.zamfira@gmail.com>
2024-05-02 23:22:55 +0300

    Client cleanup

---
+++ apps/airhockey/src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium architecto facilis ipsam nisi odit possimus quia! Blanditiis cumque ea voluptates. Corporis distinctio ea fugiat magni molestias quae quasi rerum voluptatem.</p>
+  );
+}
+
+export default App;
