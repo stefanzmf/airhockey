31c60add16e023d791036ad45bc63b02c404c51c
Stefan Zamfira <stefan.zamfira@gmail.com>
2024-05-12 20:32:44 +0300

    Feat/add query client (#3)
    * Add react-query package
    
    * Add basic query client

--- apps/airhockey/src/app.tsx
+++ apps/airhockey/src/app.tsx
@@ -1,13 +1,14 @@
-import Router from './Router';
 import { CssBaseline } from '@mui/material';
 
+import Router from './Router';
+import {QueryClientProvider} from './queryClient';
 
 export function App() {
   return (
-    <>
+    <QueryClientProvider>
       <CssBaseline />
       <Router />
-    </>
+    </QueryClientProvider>
   );
 }
 
