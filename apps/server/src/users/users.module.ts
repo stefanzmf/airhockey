ceac9c6eaa3cd98415c25b31129effa31eff8c76
StefanZ <stefan.zamfira@gmail.com>
2024-05-22 16:46:58 +0300

    Add users gateway and support for emiting active users count

--- apps/server/src/users/users.module.ts
+++ apps/server/src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { UsersGateway } from './users.gateway';
+
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersGateway]
 })
 export class UsersModule {}
